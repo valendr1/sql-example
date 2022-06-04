a9142ff31ae51f43d6540113700e4f5e7a6f6686
Valery <valerijvaler931@gmail.com>
2022-06-05 03:53:16 +0500

    change DatabaseName

--- src/main/java/db/DataSourceProvider.java
+++ src/main/java/db/DataSourceProvider.java
@@ -14,7 +14,7 @@ public enum DataSourceProvider {
             ds = new PGSimpleDataSource();
             ds.setServerNames(new String[] {"localhost"});
             ds.setPortNumbers(new int[] {5432});
-            ds.setDatabaseName("postgres");
+            ds.setDatabaseName("coin_keeper");
             ds.setUser("postgres");
             ds.setPassword("postgres");
         }
